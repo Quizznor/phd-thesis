9a90ae60d03cea8444d2daffb5f5672f65f22633
Paul Filip <paul.filip@kit.edu>
2023-12-18 14:36:08 +0000

    Finish monit reader

--- bin/utils/Auger/SD/Monitoring.py
+++ bin/utils/Auger/SD/Monitoring.py
@@ -1,32 +1,61 @@
 import typing
-from ... import create_stream_logger
-monitoring_logger = create_stream_logger("Auger.SD.monit")
-del create_stream_logger
-
 from ...binaries import np
 import uproot
-monitoring_logger.info("import uproot")
 
 class Monit():
 
     monit_path = '/cr/auger02/Prod/monit/Sd/'
 
-    def __init__(self) -> None :
+    def __init__(self, year : int, month : int, day : int, starting_branch : str = "SDMonCal/SDMonCalBranch", monit_file : str = None, IDs : np.ndarray = None) -> None :
 
-        self.__stream = uproot.open(f"{self.monit_path}/2023/11/mc_2023_11_18_00h00.root:SDMonCal")
+        if monit_file is None:
+            self.monit_file = f"{self.monit_path}/{year}/{month}/mc_{year}_{month}_{day}_00h00.root"
+        else:
+            self.monit_file = monit_file
+        
+        self.starting_branch = starting_branch
+        self.__stream = uproot.open(f"{self.monit_file}:{starting_branch}")
+        self.IDs = self.__stream['fLsId'].array() if IDs is None else IDs
 
-    def __getattr__(self, name: str) -> typing.Any :
 
-        hits = [1 if name in key else 0 for key in self.__stream.keys()]
+    def __getattr__(self, name: str) -> typing.Union[typing.Callable, set] :
+
+        hits = [True if name in key else False for key in self.__stream.keys()]
 
         if sum(hits) == 0:
-            print("Available keys are:")
-            for key in self.__stream.keys(): print(key.split('/')[-1].split('[')[0])
+            print(self)
             raise KeyError(f"'{name}' not found in online monitoring!")
         elif sum(hits) != 1:
-            raise ValueError("Fetching of entire branches is not encouraged!")
+
+            first_key = self.__stream.keys()[np.argmax(hits)].split('/')[-1]
+            print(f"Multiple entries found for {name}, returning first occurence: {first_key}")
+            return Monit(..., ..., ..., 'SDMonCal/' + first_key, self.monit_file, self.IDs)
+
+            # raise ValueError("Fetching of entire branches is not encouraged!")
         
         else:
-            key = self.__stream.keys()[np.argmax(hits)]
-            IDs = self.__stream['SDMonCalBranch/fLsId'].array()
-            return lambda x: np.array(self.__stream[key].array())[np.array(np.argwhere(IDs == x)).flatten()]
+            if name.lower() in ['id', 'flsid', 'ids']: return set(self.IDs)
+        
+        return lambda x: np.array(self.__stream[self.__stream.keys()[np.argmax(hits)]].array())[np.array(np.argwhere(self.IDs == x)).flatten()]
+    
+    
+    def __str__(self) -> str : 
+
+        s  = f"File: {self.monit_file}\n"
+        s += f"on branch: {self.starting_branch}\n"
+        s += "\n".join(self.format_keys(self.__stream.keys()))
+
+        return s
+    
+     
+    def format_keys(self, keys : list) -> list :
+
+        for i, key in enumerate(keys):
+            k = key.split('/') 
+            n_indent = len(k)
+            indent = "  " * n_indent
+            mod = "-:" if n_indent != 0 else ""
+            key = k[-1].split('.')[-1]
+            keys[i] = f"{indent}{mod} {key}"
+
+        return keys
