4ca9b81a8f81877729bf782012e7d5ab154763b0
Paul Filip <paul.filip@kit.edu>
2024-01-23 16:57:33 +0000

    Peak finding algorithm

--- Projects/SSDOnlineCalib/CheckMonitHistos/SDFileReader.cxx
+++ Projects/SSDOnlineCalib/CheckMonitHistos/SDFileReader.cxx
@@ -1,45 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include "IoSd.h"
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//
-// // command line for all files of 1, 2 and 3 may as input:
-// // [shell]> ./analyse.exe $(DATAPATH)/2002/05/eb_2002_05_0[123]*.root
-// // command line for all inputs:
-// // [shell]> ./analyse.exe $(DATAPATH)/*/*/*.root
-// /**************************************************************************/
-
-constexpr int GetError(const IoSdStation& station)
-{
-  return station.Error & 0xff;
-}
 
 int main(int argc, char *argv[]) {
 
   EventPos pos; IoSd input(argc - 1, argv + 1);
+  UInt_t nData = 0;
 
+  // make this multi-threaded?
   for (pos = input.FirstEvent(); pos < input.LastEvent(); pos = input.NextEvent())
   {
     IoSdEvent event(pos);
+    ofstream outFile("out_new.txt", ios_base::app);
     
     const vector<IoSdStation>& stations = event.Stations;
+
     for (const IoSdStation& station : stations)
     {
-      if (!station.IsUUB) continue;
+      const UInt_t stationId = station.id();
+      if (stationId != 56) continue;                                  // skip stations that are not NuriaJr
+      if (!station.IsUUB) continue;                                   // skip non-UUB stations, unneccesary
       const IoSdCalib* const stationCalib = station.calib();
-      if (stationCalib->Version <= 259) continue;
+      if (stationCalib->Version <= 262) continue;                     // ensure SSD Histograms are present
       const IoSdHisto* const calibrationHistograms = station.histo();
-      if (calibrationHistograms->type != 1) continue;
+      if (calibrationHistograms->type != 1) continue;                 // ensure SSD Histograms are normal
 
-      const UShort_t* const ssdPeakHisto = calibrationHistograms->Peak3;
+      nData += 1;
+      const UInt_t startSecond = stationCalib->StartSecond;
+      const UInt_t endSecond = startSecond + stationCalib->EndSecond;
+      const UInt_t average = 0.5 * (startSecond + endSecond);
 
-      for (unsigned int i = 0; i < sizeof(IoSdHisto::Peak3)/sizeof(UShort_t); i++)
-      {
-        cout << ssdPeakHisto[i] << " ";
-      }
+      outFile << stationId << " " << average << " ";
 
-      cout << '\n';
+      // const UShort_t* const ssdPeakHisto = calibrationHistograms->Peak3;
+      const UShort_t* wcdPeakHisto[kIoSd::NPMT][150] = {{calibrationHistograms->Peak}};
+    //   for (unsigned int i = 0; i < sizeof(IoSdHisto::Peak3)/sizeof(UShort_t); i++)
+    //   {
+    //     outFile << ssdPeakHisto[i] << " ";
+    //   }
+    //   outFile << "\n";
+    //   // break;
     }
+    // break;
   }
+  cout << "wrote " << nData << " data points to disk" << '\n';
   return 0;
 }
---
+++ Projects/TestingWorld/test_increment.cxx
@@ -0,0 +1,22 @@
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    const short int normalBins = 100;
+    const short int step = 2;
+    const short int stridedBins = 50;
+    const short int stride = 4;
+
+    std::ofstream outFile("binning.txt", std::ios_base::app);
+
+    for (short i = 0; i < normalBins; ++i)
+    {
+        outFile << i << ' ' << step*i << '\n';
+    }
+    for (short i = 0; i <= stridedBins; ++i)
+    {
+        outFile << normalBins + i << ' ' << step * (normalBins + stride*i) << '\n';
+    }
+
+    return 0;
+}
