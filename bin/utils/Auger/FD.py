a0aa6974b99e386b89b9b8cb41ed559a8724310e
Paul Filip <paul.filip@kit.edu>
2024-03-01 20:36:57 +0000

    first results for pixelwise comparison

--- bin/utils/Auger/FD.py
+++ bin/utils/Auger/FD.py
@@ -32,15 +32,17 @@ def AperturePlot(ax=None, unit='mm', filterStructure=True, zorder=10) -> None:
     ax.set_ylim(-1300 * uf, 1300 * uf)
     ax.set_aspect('equal', 'box')
 
-def PixelPlot(pixel_data, cmap=plt.cm.viridis, hist_bins=50):
+def PixelPlot(pixel_data, cmap=plt.cm.viridis, hist_bins=50, unit="", cbarlabel='ADC sum', vmin=None, vmax=None):
 
     from matplotlib.patches import RegularPolygon
-    from matplotlib.colors import LogNorm
+    from matplotlib.colors import Normalize
     from matplotlib.cm import ScalarMappable
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    norm = LogNorm(vmin=np.nanmin(pixel_data), vmax=np.nanmax(pixel_data))
+    vmin = vmin if vmin is not None else np.nanmin(pixel_data)
+    vmax = vmax if vmax is not None else np.nanmax(pixel_data)
+    norm = Normalize(vmin=vmin, vmax=vmax)
     
     for ipix, pixel in enumerate(pixel_data, 1):
 
@@ -66,20 +68,23 @@ def PixelPlot(pixel_data, cmap=plt.cm.viridis, hist_bins=50):
 
         ax1.add_patch(hexagon)
 
+    hist_bins = np.linspace(vmin, vmax, hist_bins)
     n, _, _ = ax2.hist(pixel_data, bins = hist_bins, histtype='step', lw=2)
     ax2.text(0.98, 0.98,
-             f"$\mu = {np.mean(pixel_data):.2f}\,\mathrm{{ADC}}$, $\sigma = {np.std(pixel_data):.2f}\,\mathrm{{ADC}}$" + '\n' \
-             + fr"$(\uparrow, \downarrow) = ({min(pixel_data):.2f}\,\mathrm{{ADC}}, {max(pixel_data):.2f}\,\mathrm{{ADC}})$",
+             f"$\mu = {np.mean(pixel_data):.2f} {unit}$, $\sigma/\mu = {np.std(pixel_data)/np.mean(pixel_data):.3f}$" + '\n' \
+             + fr"$(\uparrow, \downarrow) = ({min(pixel_data):.0f} {unit}, {max(pixel_data):.0f} {unit})$",
              horizontalalignment='right',
              verticalalignment='top',
              transform=ax2.transAxes,
              )
     ax2.set_ylim(0, 1.2 * max(n))
 
-    fig.colorbar(ScalarMappable(norm=norm, cmap=cmap), ax=ax1, orientation='horizontal')
+    fig.colorbar(ScalarMappable(norm=norm, cmap=cmap), ax=ax1, orientation='horizontal', label=cbarlabel)
 
     ax1.set_xlim(-15.8, 15.8)
     ax1.set_ylim(-15.8, 15.8)
     ax1.invert_yaxis()
     ax1.axis('off')
     ax1.set_aspect(20 / 22)
+
+    return ax1, ax2
