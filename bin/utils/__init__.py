d633b3da6ecade66ee5371696c592ac365c50a6c
Paul Filip <paul.filip@kit.edu>
2024-04-02 20:54:11 +0000

    refactor logging

--- bin/utils/__init__.py
+++ bin/utils/__init__.py
@@ -1,73 +1,66 @@
-__all__ = ['binaries', 'plotting', 'testing', 'Auger']
+__all__ = ['binaries', 'plotting', 'Auger']
 
+from termcolor import colored
 import logging
-from colorlog import ColoredFormatter
+import time
 import re
 
-class Formatter(ColoredFormatter):
-    """Custom logging class to allow for stylized formatting, and runtime analysis"""
+class Formatter(logging.Formatter):
+    """Custom logging class to allow for stylized formatting, and (rough) runtime analysis"""
 
     last_log = 0
-    levels = {
-    'DEBUG' : "DBUG",
-    "INFO" : "INFO",
-    "WARNING" : "WARN",
-    "ERROR" : "ERR",
-    "CRITICAL" : "CRIT"}
 
-    def format(self, record):
+    blue = staticmethod(lambda _str : colored(_str, 'blue', attrs=['bold', 'blink']))
+    green = staticmethod(lambda _str : colored(_str, 'green', attrs=['bold', 'blink']))
+    yellow = staticmethod(lambda _str : colored(_str, 'yellow', attrs=['bold', 'blink']))
+    red = staticmethod(lambda _str : colored(_str, 'red', attrs=['bold', 'blink']))
+    critical = staticmethod(lambda _str : colored(_str, 'red', attrs=['bold', 'blink']))
+    purple = staticmethod(lambda _str : colored(_str, 'magenta', attrs=['bold']))
+    levels = [blue, green, yellow, red, critical]
+
+    def __init__(self) -> None :
+        super().__init__()
+    
+    def format(self, record : logging.LogRecord) -> str :
+
+        format_time = lambda t : f"+{int(t)}ms" if t < 1000 else f"+{t/1000:.1f}s"
+        lvl = lambda _str : self.levels[record.levelno // 10 - 1](_str)
+
         delta_milliseconds = record.relativeCreated - Formatter.last_log
         Formatter.last_log = int(record.relativeCreated)
-        record.threadName = f"+{int(delta_milliseconds)}ms" if delta_milliseconds < 99999 else f'<99999ms'
-        record.threadName = record.threadName.rjust(8)
-        record.thread = f"{f'[{self.levels[record.levelname]}]': <6}"
-        record.msg = self.format_message(record.msg)
+        elapsed = format_time(delta_milliseconds).rjust(8)
+        threat = f'[{record.levelname: <7}]'
+        localtime = time.strftime('%H:%M:%S', time.localtime())
+        msg = self.highlight_imports(record.msg)
+        
+        return f"{lvl(localtime)} ({self.purple(elapsed)}) {lvl(threat)} -- {msg}"
 
-        return super().format(record)	
 
     @staticmethod
-    def format_message(msg : str) -> str :
+    def highlight_imports(msg : str) -> str :
         """filter and format specific keywords in logging messages"""
-        BOLD = '\033[1m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
+
 
         import_as_pattern = re.compile('import\s.*as\s\S*')
         import_pattern = re.compile('import\s\S*')
         if re.search(import_as_pattern, msg):
             _import, _module, _as, _alias = msg.split()
-            _module = BOLD + FAIL + _module + ENDC
-            _alias = BOLD + FAIL + _alias + ENDC
+            _module = Formatter.red(_module)
+            _alias = Formatter.red(_alias)
             msg = ' '.join([_import, _module, _as, _alias])
         elif re.search(import_pattern, msg):
             _import, *_modules = msg.split()
             _module = ''
             for _pkg in _modules:
                 _pkg = _pkg.replace(',', '')
-                _module += BOLD + FAIL + _pkg + ENDC
+                _module += Formatter.red(_pkg)
             if not _pkg == _modules[-1]:
                 _module += ', '
             msg = ' '.join([_import, _module])
         
         return msg
 
-    def __init__(self) -> None :
-        super().__init__(
-            f'{{log_color}}{{asctime}} {{thread}} {{name}}{{reset}} -- ({{bold_purple}}{{threadName}}{{reset}}) -- {{msg}}',
-            datefmt='%H:%M:%S',
-            reset=True,
-            log_colors={
-            'DEBUG' :		'bold_cyan',
-            'INFO' :    	'bold_green',
-            'WARNING' : 	'bold_yellow',
-            'ERROR' :   	'bold_red',
-            'CRITICAL' :	'bold_blue',
-            },
-            secondary_log_colors={},
-            style='{'
-        )
-
-def create_stream_logger(name : str, loglevel=logging.DEBUG) -> logging.Logger :
+def create_stream_logger(name : str, loglevel : int = logging.DEBUG) -> logging.Logger :
     """create a simple logger that streams to stdout"""
 
     logger = logging.getLogger(f"{name:<14}")
