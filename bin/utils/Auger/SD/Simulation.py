21e4f8259b2098a90a269f3995fb50d23eb5a15e
Paul Filip <paul.filip@kit.edu>
2025-01-22 18:34:23 +0000

    make simulations actually work

--- bin/utils/Auger/SD/Simulation.py
+++ bin/utils/Auger/SD/Simulation.py
@@ -1,30 +1,38 @@
+from ...binaries.binary_tools import ProgressBar
 from ... import CONSTANTS
 from pathlib import Path
+import subprocess
+import json
 import stat
 import os
 
 class Simulation():
 
-    CROFFLINE = 'cr/data01/filip/offline/install'
+    CROFFLINE = '/cr/data01/filip/offline/install'
+    CRSRC = '/cr/users/filip/bin/utils/Auger'
     CRWORK = '/cr/work/filip/Simulations'
 
-    QUEUE = 1
-    RETHROWS = 1
+    QUEUE = "1"
+    RETHROWS = "1"
     MEMORY = "1G"
-    MAX_IDLE = 150
-    PRIMARY = 'proton'
-    ENERGY = '16.5_17'
-    MAX_MATERIALIZE = 150
+    MAX_IDLE = "150"
+    PRIMARY = "proton"
+    ENERGY = "16.5_17"
+    NPARTICLES = "30000"
+    MAX_MATERIALIZE = "150"
 
-    def __init__(self, name: str, offline: str, **kwargs: dict):
+    def __init__(self, name: str, offline: str, src: str, **kwargs: dict):
 
         # make file system
         self.path = Path(f"{self.CRWORK}/{name}")
         self.path.mkdir(parents=True, exist_ok=True)
         self.kwargs, QUEUE = self.get_simulation_kwargs(kwargs)
+        self.offline_src = f"{self.CROFFLINE}/{offline}/set_offline_env.sh"
 
-        for dir in ['src', 'out', 'sim', 'log']:
+        for dir in ['src', 'sim', 'log']:
             Path(self.path / dir).mkdir(parents=True, exist_ok=True)
+        for dir in ['out', 'dat']:
+            Path(self.path / dir / self.kwargs['PRIMARY'] / self.kwargs['ENERGY']).mkdir(parents=True, exist_ok=True)
 
         # make run .sub file
         sub_path = self.path / "run.sub"
@@ -42,23 +50,26 @@ class Simulation():
         # make run  .sh file
         sh_path = self.path / "run.sh"
         with sh_path.open("w", encoding="utf-8") as sh:
-            sh.write("#/bin/bash\n")
-            sh.write(f"\nsource {self.CROFFLINE}/{offline}/set_offline_env.sh\n")
-            sh.write("./src/userAugerOffline --bootstrap $2\n")
-            sh.write("rm -rf *.root src/*.root *.dat src/*.dat $2")
+            sh.write("#!/bin/bash\n")
+            sh.write(f"\nsource {self.offline_src}\n")
+            sh.write("./userAugerOffline --bootstrap $1\n")
+            sh.write("rm -rf *.root*.dat $1")
 
         sh_path.chmod(sh_path.stat().st_mode | stat.S_IEXEC)
 
         # make run  .py file
-        os.system(f"cp /cr/users/filip/bin/utils/Auger/SD/run.py {self.path}")
+        os.system(f"cp {self.CRSRC}/SD/run.py {self.path}")
         
+        # set up source
+        os.system(f"cp {src}/* {self.path / 'src'}")
+
 
     def get_simulation_kwargs(self, kwargs: dict) -> dict:
 
-            PRIMARY = kwargs.get("PRIMARY", self.PRIMARY)
-            ENERGY = kwargs.get("ENERGY", self.ENERGY)
-            RETHROWS = kwargs.get("RETHROWS", self.RETHROWS)
-            QUEUE = kwargs.get("QUEUE", self.QUEUE)
+            QUEUE, args = kwargs.get("QUEUE", self.QUEUE), []
+            for arg in ["PRIMARY", "ENERGY", "RETHROWS", "NPARTICLES"]:
+                args.append(kwargs.get(arg, getattr(self, arg)))
+                kwargs[arg] = kwargs.get(arg, getattr(self, arg))
 
             kwargs['max_idle'] = kwargs.get("max_idle", self.MAX_IDLE)
             kwargs['max_materialize'] = kwargs.get("max_materialize", self.MAX_MATERIALIZE)
@@ -66,21 +77,87 @@ class Simulation():
 
             # num_cores, etc...
 
-            kwargs['arguments'] = kwargs.get("arguments", f'"{PRIMARY} {ENERGY} {RETHROWS} $(Process)"')
-            kwargs['JobBatchName'] = f"{self.path.name}_{kwargs['PRIMARY']}_{kwargs['ENERGY']}"
+            kwargs['JobBatchName'] = f"{self.path.name}_{args[0]}_{args[1]}"
+            kwargs['arguments'] = kwargs.get("arguments", f'"{self.path.name} {" ".join(args)} $(Process)"')
             
             kwargs['executable'] = "./run.py"
-            kwargs['error'] = self.path / f"log/{kwargs['JobBatchName']}-$(Process).err"
-            kwargs['output'] = self.path / f"log/{kwargs['JobBatchName']}-$(Process).out"
-            kwargs['log'] = self.path / f"log/{kwargs['JobBatchName']}-$(Process).log"
+            kwargs['error'] = str(self.path / f"log/{kwargs['JobBatchName']}-$(Process).err")
+            kwargs['output'] = str(self.path / f"log/{kwargs['JobBatchName']}-$(Process).out")
+            kwargs['log'] = str(self.path / f"log/{kwargs['JobBatchName']}-$(Process).log")
 
             return kwargs, QUEUE
 
 
-    def cleanup(self, log: bool=True, out: bool=True, err: bool=True, data: bool=False):
-        
-        if log: os.system(f"rm -rf {self.path / 'log/*.log'}")
-        if out: os.system(f"rm -rf {self.path / 'log/*.out'}")
-        if err: os.system(f"rm -rf {self.path / 'log/*.err'}")
+    def process(self, executable: str= None) -> None:
 
-        if data: os.system(f"rm -rf {self.path / 'out/*'}")
+        if executable is None:
+            executable = f"{self.CRSRC}/ADST/AdstReader"
+
+        data_dir = self.path / f"out/{self.kwargs['PRIMARY']}/{self.kwargs['ENERGY']}/"
+        for file in ProgressBar(os.listdir(data_dir)):
+            subprocess.run(" ".join([f". {self.offline_src} &&", executable, f"{data_dir}/{file}"]), 
+                           shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+    def cleanup(self, log: bool=True, out: bool=False, dat: bool=False):
+        
+        if log: os.system(f"rm -rf {self.path / 'log/*'}")
+        if out: os.system(f"rm -rf {self.path / 'out/*'}")
+        if dat: os.system(f"rm -rf {self.path / 'dat/*'}")
+
+
+    def run(self, proc_no: int) -> int:
+        this_args = self.kwargs["arguments"].replace("$(Process)", str(proc_no))[1:-1]
+        return subprocess.run([f"{self.path / 'run.py'}", *this_args.split(' ')])
+
+
+    def __str__(self) -> str:
+        return json.dumps(self.kwargs, indent=2)
+
+
+    def status(self) -> None:
+
+        print("*.root files present:")
+        self.tree_view(self.path / "out")
+
+        print("\n*.csv files present:")
+        self.tree_view(self.path / "dat")
+
+        print()
+        print(self)
+
+
+    @staticmethod
+    def tree_view(path: str) -> None:
+
+        primaries = os.listdir(path)
+        energies = set()
+        [[energies.add(e) for e in os.listdir(path/p)] for p in primaries]
+
+        for p in primaries:
+            print(f"  {p}")
+            for dir in energies:
+                print(f"    {dir}: {len(os.listdir(path/p/dir)): >5} files")
+
+
+    @classmethod
+    def help(cls) -> None:
+        
+        print('"""')
+        print("Wrapper around Offline Simulation software")
+        print()
+        print("\tRequired arguments:")
+        print("\t\tname                            -- directory name in /cr/work/filip/Simulations")
+        print("\t\toffline                         -- offline build in /cr/data01/filip/Offline")
+        print("\t\tsrc                             -- source directory with bootstrap, userAugerOffline")
+        print()
+        print("\tKeyword arguments:")
+        print("\t\tQUEUE                == 1       -- how many showers to simulate (default: 1)")
+        print("\t\tRETHROWS             == 1       -- how many rethrows for each shower (default: 1)")
+        print("\t\tMEMORY               == 1G      -- how much RAM to request for each job")
+        print("\t\tMAX_IDLE             == 150     -- max jobs to put into idle queue")
+        print("\t\tPRIMARY              == proton  -- which primary particle to simulate")
+        print("\t\tENERGY               == 16.5_17 -- energy (bin) of the primary particle")
+        print("\t\tNPARTICLES           == 30000   -- for quick/dirty simulation tests")
+        print("\t\tMAX_MATERIALIZE      == 150     -- max jobs to put into queue")
+        print('"""')
--- bin/utils/Auger/SD/run.py
+++ bin/utils/Auger/SD/run.py
@@ -3,16 +3,17 @@
 import os, sys
 import subprocess
 
-def determine_source_file(arguments) -> list[str]:
+CRWORK = "/cr/work/filip/Simulations"
 
-    is_valid = (
-        lambda s: s.startswith("DAT")
-        and not s.endswith(".long")
-        and not s.endswith(".lst")
-        and not s.endswith(".gz")
-    )
+def prepare_bootstrap(arguments, seed) -> bool:
 
-    primary, energy, rethrows, proc_no = arguments
+    is_valid = (lambda s: s.startswith("DAT")
+                and not s.endswith(".long")
+                and not s.endswith(".lst")
+                and not s.endswith(".gz"))
+
+    name, primary, energy, rethrows, n_particles, proc_no = arguments
+    
     hadron_prefix = "prague" if energy < "18.5_19" else "napoli"
     photon_prefix = "prague"
     data_dirs = {
@@ -20,49 +21,42 @@ def determine_source_file(arguments) -> list[str]:
         "proton": f"/lsdf/auger/corsika/{hadron_prefix}/EPOS_LHC/proton/{energy}/",
     }
 
+
     source_dir = data_dirs[primary]
     files = list(filter(is_valid, os.listdir(source_dir)))
-    return source_dir + files[proc_no], files[proc_no], int(rethrows), int(proc_no)
+    target_dir = f"/cr/work/filip/Simulations/{name}"
+    source_file = files[int(proc_no)]
+    target_file = f"{target_dir}/out/{primary}/{energy}/{source_file.split('.')[0]}_{seed}.root"
+    target_bootstrap = f"{target_dir}/sim/bootstrap_{seed:06}.xml"
 
-
-def prepare_bootstrap(arguments) -> None:
-    raise NotImplementedError
-
-if __name__ == "__main__":
-
-    FILE, NAME, RETHROWS, PROC = determine_source_file(sys.argv[1:])
-
-    for DSEED, PSEED in zip(range(0, RETHROWS), range(1, RETHROWS+1)):
-        src_bootstrap = "./src/bootstrap.xml"
-
-
-for _ in range(NUM_RETHROWS):
-    target_file = f"/cr/work/filip/SSDTriggerEfficiency/run01/{name.split('.')[0]}_{DSEED:06}.root"
-    target_bootstrap = (
-        f"/cr/work/filip/Simulations/Bootstraps/bootstrap_{proc_no}_{DSEED}.xml"
-    )
-
-    # Prepare bootstrap
     replacements = {
-        "@INPUTFILE@": file,
-        "@NPARTICLES@": "30000",
+        "@INPUTFILE@": f"{source_dir}/{source_file}",
+        "@NPARTICLES@": n_particles,
         "@OUTPUTFILE@": target_file,
         "@DETECTORSEED@": f"{DSEED:06}",
         "@PHYSICSSEED@": f"{PSEED:06}",
-        "@PATTERNPATH@": "*.part" if file.endswith(".part") else "*",
-        "@GROUNDDATA@": "(1).part" if file.endswith(".part") else "(1)",
+        "@PATTERNPATH@": "*.part" if source_file.endswith(".part") else "*",
+        "@GROUNDDATA@": "(1).part" if source_file.endswith(".part") else "(1)",
     }
 
-    DSEED += 1
-    PSEED += 1
-
     with open(target_bootstrap, "w") as target:
-        # with open('../SdSimulationReconstructionUpgrade/bootstrap.xml', 'r') as source:
-        with open("bootstrap.xml", "r") as source:
+        with open(f"{CRWORK}/{name}/src/bootstrap.xml", "r") as source:
             for line in source.readlines():
                 try:
                     target.write(replacements[line.strip()] + "\n")
                 except KeyError:
                     target.write(line)
+                    
+    return target_bootstrap
 
-    subprocess.call(["./run_simulation.sh", target_bootstrap, target_file])
+
+if __name__ == "__main__":
+
+    arguments = (NAME, PRIMARY, ENERGY, RETHROWS, N_PARTICLES, PROC) = sys.argv[1:]
+    os.chdir(f"{CRWORK}/{NAME}/src/")
+
+    for DSEED, PSEED in zip(range(0, int(RETHROWS)), range(1, int(RETHROWS)+1)):
+        DSEED, PSEED = f"{DSEED:06}", f"{PSEED:06}"    
+
+        if target_bootstrap := prepare_bootstrap(arguments, DSEED):
+            subprocess.run([f"{CRWORK}/{NAME}/run.sh", target_bootstrap])
